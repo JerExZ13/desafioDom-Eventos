06f5f80b25e433c81d2959fde54980930bfcf8d9
Jeremias Ramos <Exzed13@hotmail.com>
2022-09-11 19:00:18 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const productos = []
+const carrito = []
+
+class Producto{
+    constructor(id, nombre, precio, img, desc = ''){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.img = img 
+        this.desc = desc 
+    }
+    desplegarProductos(){
+        const card = `
+            <div class="card">
+                <p>${this.nombre}</p>
+                <div>
+                    <img class='imgProducto' src=${this.img} alt="foto del producto"/>
+                </div>
+                <div>
+                    <p>$${this.precio}</p>
+                </div>
+                <div class="btn-container">
+                    <button id=${this.id} class='btnAgregar'>AGRGEGAR AL CARRITO</button>
+                </div>
+            </div>
+        `
+        const container = document.getElementById('container')
+        container.innerHTML += card
+    }
+}
+
+let prod1 = new Producto('001', 'Moka', 3000, './img/Postre1.jpg')
+let prod2 = new Producto('002', 'Muffin', 150, './img/Postre2.jpg')
+let prod3 = new Producto('003', 'Chocotorta', 2500, './img/Postre3.jpg')
+let prod4 = new Producto('004', 'Tarta de frutos rojos', 3200, './img/Postre4.jpg')
+let prod5 = new Producto('005', 'Torta Musse', 4150, './img/Postre5.jpg')
+let prod6 = new Producto('006', 'Tarta de chocolate', 3200, './img/Postre6.jpg')
+
+productos.push(prod1, prod2, prod3, prod4, prod5, prod6)
+
+console.log(productos)
+
+productos.forEach(e => {
+    e.desplegarProductos()
+})
+
